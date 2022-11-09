6496056fb1d25e88ace21b86e93bf9d6865bab93
Simone Giolai <simonegiolai74@gmail.com>
2022-11-09 20:44:10 +0100

    Update

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-function signIn(){
-    let utenti = JSON.parse(localStorage.getItem('utenti'));
-    const utente = {
-        username: document.getElementById("username").value,
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value,
-        passwordConfirm: document.getElementById("passwordConfirm").value,
+let z = 0;
+function singIn(){
+    let persons = JSON.parse(localStorage.getItem('persons'));
+    if(persons === null){
+        persons = [];
+    }else{
+        let persons = JSON.parse(localStorage.getItem('persons'));
     }
-    utenti.push(utente);
-    localStorage.setItem("utenti", JSON.stringify(utenti));
-    alert("User created");
+    if (document.getElementById("password").value == document.getElementById("passwordConfirm").value) {
+        const person = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+            passwordConfirm: document.getElementById("passwordConfirm").value
+        }
+        persons.push(person);
+        localStorage.setItem('persons', JSON.stringify(persons));
+
+    }else{
+        alert("Password and password confirm are not the same");
+    }
+
 }
 
-function login(){
-    const utenti = JSON.parse(localStorage.getItem('utenti'));
-    if(utenti == null){
-        alert("No user found, sign in first");
+function logIn(){
+    let username = document.getElementsByName('user')[0].value;
+    let password = document.getElementsByName('pw')[0].value;
+    let persons = JSON.parse(localStorage.getItem('persons'))
+    let check = false;
+
+    if (persons == null){
+        alert('No user found, sign in first');
     }else{
-        for (let i = 0; i < utenti.length; i++){
-            if (utenti[i].username == document.getElementById("username").value && utenti[i].password == document.getElementById("password").value){
-                alert("Welcome " + utenti[i].username);
-                window.location.href = "welcome.html";
-            }else{
-                alert("Wrong username or password");
+        for (let i = 0; i < persons.length; i++){
+            if (username == persons[i].username && password == persons[i].password){
+                z = i
+                check = true
             }
         }
+        if (check == true){
+            alert('Welcome ' + persons[z].name + ' ' + persons[z].surname);
+            window.location.href = 'html/list.html';
+        }else{
+            alert('Wrong username or password');
+        }
     }
+
 }
